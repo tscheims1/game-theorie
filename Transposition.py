c47fab3c78feda78db552b4ed214c252b45cb6b9
tscheims <james.schuepbach@gmail.com>
2016-04-05 22:29:51 +0200

    fixed bugs

--- Transposition.py
+++ Transposition.py
@@ -1,12 +1,25 @@
-import numpy as np
+
 class Transposition(object):
 	def __init__(self):
+		#hash-map
 		self.dict = {}
-	def add(self,board,node):
-		if(self.dict[np.array_str(board)] == None)
-			self.dict[np.array_str(board)] = board
+	
+	#Return 0 if the node can clipped
+	def addAndCheck(self,board,node):
+		obj = self.dict.get(str(board))
+		if(obj == node):
+			return 1
+		if(obj == None):
+			self.dict[str(board)] = node
+			return 1
+		else:
+			return 0
 
-	def isCalculated(self,board,node):
-		obj = self.dict[np.array_str(board)]
-		return obj != node and obj !=None
+
+trans = Transposition()
+a = [1,2,3,4]
+print (str(trans.addAndCheck(a,a)))
+print (str(trans.addAndCheck(a,1)))
+print (str(trans.addAndCheck(a,a)))
+
 
